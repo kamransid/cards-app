6e82a20bdcb87a0fe64382f7e8d4f76bb693172e
kamran <kamranprogrammer28@gmail.com>
2020-10-19 15:14:29 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      name:''
+    }
+  }
+    render(){
+      return (
+        <div>Hi</div>
+      )
+    }
+}
+exports default App;
